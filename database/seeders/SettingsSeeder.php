0f87df7eb16acac78cbbadbaae74e3e1b88784f8
Collins Kiplangat Rono <ronocollins2000@gmail.com>
2025-05-23 11:11:46 +0300

    changes before receipt

--- database/seeders/SettingsSeeder.php
+++ database/seeders/SettingsSeeder.php
@@ -15,8 +15,8 @@ class SettingsSeeder extends Seeder
     public function run()
     {
         $data = [
-            ['key' => 'app_name', 'value' => 'Laravel-POS'],
-            ['key' => 'currency_symbol', 'value' => '$'],
+            ['key' => 'app_name', 'value' => 'Pharmacy-POS'],
+            ['key' => 'currency_symbol', 'value' => 'KES'],
         ];
 
         foreach ($data as $value) {
--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -2,5 +2,5 @@
     <div class="float-right d-none d-sm-block">
       <b>{{ __('common.Version') }}</b> 3.0.3
     </div>
-    <strong>{{ __('common.Copyright') }}Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong> {{ __('common.all_reserved') }}
+    <strong>Copyright &copy; 2025 Pharmacy POS </strong> {{ __('common.all_reserved') }}
   </footer>
